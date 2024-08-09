96a768c638b61bbee1ede2ae2d2b2081754bb547
rcalizayapa <rcalizayapa@unjbg.edu.pe>
2024-08-09 04:54:14 -0500

    Se corrigieron algunos errores

--- funciones.h
+++ funciones.h
@@ -1,28 +1,40 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
-#include <iostream>
+
+#include<iostream>
 #include <ctime>
 #include <conio.h>
 #include <string>
-#include <stdio.h>
+#include <list>
 
 using namespace std;
-enum inicio{
-    ejecutar,
-    salir,
-    finalizar
+struct estudiante{
+int id;
+
+string nombre;
+string apellido;
+
+int edad;
+string estatura;
+string escuela;
+string correo;
+string codigo;
+string whatsapp;
+string ingreso;
+string salida;
+string llave;
 };
-int main();
-void mostrarbienvenido(int kbhit());
-string funcioninicio(inicio boton);
+
+
+extern 
+list<estudiante> estudiantes;
 //Funcion para limpiar la pantalla
 void limpiarPantalla();
 string obtenerHoraActual();
-
 //Funcion para obtener ingresar los datos de los estudiantes
 void ingresarDatosEstudiantes();
 //Funcon para mostrar los datos de los estudiantes
-void mostrarDatosEstudiante();
+void mostrarDatosEstudiante(const estudiante& est);
 //Funcion para introducir y reconocer la llave del estufiante
 void ejecutarPrograma();
 //Funcion para mostrar el menu secundario de opciones

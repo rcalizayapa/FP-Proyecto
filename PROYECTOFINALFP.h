0eab7397fa37cac8562fb745a5b4dde7188f7c62
afloresh1 <afloresh@unjbg.edu.pe>
2024-08-09 02:30:00 -0500

    Se separaron las funciones para el uso del .h

---
+++ PROYECTOFINALFP.h
@@ -0,0 +1,14 @@
+#ifndef PROYECTOFINALFP_H_
+#define PROYECTOFINALFP_H_
+#include <iostream>
+#include <ctime>
+#include<conio.h>
+#include<string>
+#include<stdio.h>
+
+using namespace std;
+
+void mostrarbienvenido(int kbhit());
+string funcioninicio(Inicio boton);
+
+#endif
